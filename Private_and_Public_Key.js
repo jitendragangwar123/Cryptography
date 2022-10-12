a583fa238170dcd3668bbdd60da8bfb0cac696ac
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-10-12 19:15:33 +0530

    initial commit

--- Private_and_Public_Key.js
+++ Private_and_Public_Key.js
@@ -1,9 +1,9 @@
-###Private Key :-
+###PrivateKey :-
           ```shell
           Private keys can be used for both encryption and decryption.
           Private keys are shared between the sender and the receiver.
           ```
-###Public Key :-    
+###PublicKey :-    
           ```shell
           Public keys are used only for the purpose of encrypting the sensitive data.  
           Public keys can be freely circulated among multiple users.

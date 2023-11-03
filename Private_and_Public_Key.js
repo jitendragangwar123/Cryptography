dddc71d708f6c583dd1067cec334e06e62108cc5
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-03 22:02:49 +0530

    Update Private_and_Public_Key.js

--- Private_and_Public_Key.js
+++ Private_and_Public_Key.js
@@ -1,10 +1,9 @@
-##PrivateKey :-
-          ```shell
-          Private keys can be used for both encryption and decryption.
-          Private keys are shared between the sender and the receiver.
-          ```
-##PublicKey :-    
-          ```shell
-          Public keys are used only for the purpose of encrypting the sensitive data.  
-          Public keys can be freely circulated among multiple users.
-          ```
+/*
+PrivateKey 
+    Private keys can be used for both encryption and decryption.
+    Private keys are shared between the sender and the receiver.
+         
+PublicKey :-    
+    Public keys are used only for the purpose of encrypting the sensitive data.  
+    Public keys can be freely circulated among multiple users.
+*/

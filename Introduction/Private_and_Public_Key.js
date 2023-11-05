c1221574bb6287326695798024465e078a154b4f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-05 16:46:40 +0530

    feat :Private_and_Public_Key Demo link added

--- Introduction/Private_and_Public_Key.js
+++ Introduction/Private_and_Public_Key.js
@@ -1,9 +1,19 @@
 /*
-PrivateKey 
+PrivateKey :-
     Private keys can be used for both encryption and decryption.
     Private keys are shared between the sender and the receiver.
          
 PublicKey :-    
     Public keys are used only for the purpose of encrypting the sensitive data.  
     Public keys can be freely circulated among multiple users.
+*/
+
+/*
+ - User Sign the Message using his own private key.
+ - Miner verify the transaction using verification function which takes user's Public key,
+   Message and signature hash.
+*/
+
+/*
+ Demo :- https://tools.superdatascience.com/blockchain/public-private-keys/keys
 */

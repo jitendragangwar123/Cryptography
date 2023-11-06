6f1c7fdcd52655cb666161495076678a524a3c8c
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-07 00:00:08 +0530

    feat:find the color sol added

--- CryptographicHashes/Brute_Force_Hashing.js
+++ CryptographicHashes/Brute_Force_Hashing.js
@@ -31,4 +31,34 @@ qwerty	          0x65e84b...2337c5
 111111	          0xbcb15f...09802a
 */
 
+//index.js
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+
+// the possible colors that the hash could represent
+const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
+
+// given a hash, return the color that created the hash
+function findColor(hash) {
+    return COLORS.find(color=>
+        toHex(sha256(utf8ToBytes(color)))===toHex(hash));
+}
+
+module.exports = findColor;
+
+//test.js
+const {assert} = require('chai');
+const findColor = require('../index');
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
+
+describe('findColor', () => {
+    COLORS.forEach((color) => {
+        it(`should work for ${color}`, () => {
+            assert.equal(findColor(sha256(utf8ToBytes(color))), color);
+        });
+    });
+});
 

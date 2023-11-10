fc345e828ee74f6a799ef28c6dd6796818840564
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-11-10 17:01:07 +0530

    feat: MineTransaction.js file added

---
+++ ProofOfWork/MineTransaction.js
@@ -0,0 +1,100 @@
+/*
+Block Size :
+    1.There is a specific block size limit that cannot be exceeded.
+    2.The number of transactions that will fit inside of a block varies due to transactions being of all different sizes.
+    3.For the purposes of this exercise, we will use the MAX_TRANSACTIONS constant.
+*/
+
+//index.js
+const SHA256 = require('crypto-js/sha256');
+const TARGET_DIFFICULTY = BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+    mempool.push(transaction);
+}
+
+function mine() {
+    let transactions = [];
+    while (transactions.length < MAX_TRANSACTIONS && mempool.length > 0) {
+        transactions.push(mempool.pop());
+    }
+    const block = { id: blocks.length, transactions }
+    const hash = SHA256(JSON.stringify(block));
+    blocks.push({ ...block, hash })
+}
+
+module.exports = {
+    TARGET_DIFFICULTY,
+    MAX_TRANSACTIONS,
+    addTransaction,
+    mine,
+    mempool,
+    blocks,
+};
+
+//test.js
+const { assert } = require('chai');
+const { mine, blocks, mempool, addTransaction } = require('../index');
+const SHA256 = require('crypto-js/sha256');
+
+describe('mine', () => {
+    describe('with 5 mempool transactions', () => {
+        before(() => {
+            for(let i = 0; i < 5; i++) {
+                addTransaction({ sender: 'bob', to: 'alice' });
+            }
+        });
+        describe('after mining', () => {
+            before(() => {
+                mine();
+            });
+            it('should add to the blocks', () => {
+                assert.equal(blocks.length, 1);
+            });
+            it('should store the transactions on the block', () => {
+                assert.equal(blocks[blocks.length - 1].transactions.length, 5);
+            });
+            it('should clear the mempool', () => {
+                assert.equal(mempool.length, 0);
+            });
+        });
+    });
+    describe('with 15 mempool transactions', () => {
+        before(() => {
+            for (let i = 0; i < 15; i++) {
+                addTransaction({ sender: 'bob', to: 'alice' });
+            }
+        });
+        describe('after mining', () => {
+            before(() => {
+                mine();
+            });
+            it('should add to the blocks', () => {
+                assert.equal(blocks.length, 2);
+            });
+            it('should store the transactions on the block', () => {
+                assert.equal(blocks[blocks.length - 1].transactions.length, 10);
+            });
+            it('should reduce the mempool to 5', () => {
+                assert.equal(mempool.length, 5);
+            });
+            describe('after mining again', () => {
+                before(() => {
+                    mine();
+                });
+                it('should add to the blocks', () => {
+                    assert.equal(blocks.length, 3);
+                });
+                it('should store the transactions on the block', () => {
+                    assert.equal(blocks[blocks.length - 1].transactions.length, 5);
+                });
+                it('should clear the mempool', () => {
+                    assert.equal(mempool.length, 0);
+                });
+            });
+        });
+    });
+});
